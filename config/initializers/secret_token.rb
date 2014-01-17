cdb513dca274552b88132d188757b94384db23d3
Daniel Smith <jellymann@gmail.com>
2014-01-17 11:49:45 +0200

    Created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeaTrolly::Application.config.secret_token = '269ee7ef915771e638932d956a55721c5ebf87c60c308b2961e9503a5e59fd55dfde875ced8297832fa15fad6cf3cf5b5764af5fc1a3528b8e73d6c4682b9ae3'
