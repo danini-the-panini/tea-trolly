22f73ae2f596db9309021842ae43e332dd709bb3
Daniel Smith <jellymann@gmail.com>
2014-01-19 23:53:05 +0200

    Got Google oauth working

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+
+  validates_presence_of :name
+  validates_length_of :name, in: 8..16
+  validates_uniqueness_of :name
+  validates_format_of :name,
+    with: /^[a-zA-Z][a-zA-Z0-9 ]*[a-zA-Z0-9]$/
+
+  def self.find_for_oauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      #user.image = auth.info.image # assuming the user model has an image
+      user.save!
+    end
+  end
+end
