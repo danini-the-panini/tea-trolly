4e9b405de551bac0011abefff2afdacb2f02589f
Daniel Smith <jellymann@gmail.com>
2014-01-21 15:07:00 +0200

    Added seeds and fixed item table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Item.create(name: 'MegaLawn 3000', description: 'The ultimate in lawn products. This will change you life!', price: '599.95')
+Item.create(name: 'NotSoLawn 23', description: "The most useless piece of crap ever created for your lawn. But it's cheap!", price: '4.95')
+Item.create(name: 'Mow Mow Mow Your Lawn XT', description: 'Bring the fun back into mowing with this kiddies-sized mower!', price: '2999.95')
